d4bb87c9e7e35bb86bb2671402d78532b13e9387
Berinim <julien.reb.jr@gmail.com>
2019-01-11 16:59:35 +0100

    Ajout du tpnote, l'ajout d'une image et d'une phrase tourne et me sort une erreur bizarre -> give up

--- api/controllers/CowsayController.js
+++ api/controllers/CowsayController.js
@@ -16,11 +16,11 @@ module.exports = {
     console.debug('Got '+count+' sentences in database');
     let s = await Sentences.find().limit(1).
       skip(Math.floor(Math.random() * Math.floor(count)));
-    let sentence = "Random Message";
+    let sentence = "Aled";
     if(s.length > 0) {
       sentence = s[0].sentence;
     }
-    return res.view('cowsay', { cow: cowsay.say({
+    return res.view('cowsay', { picture: s[0].picture, cow: cowsay.say({
       f: process.env.COW || 'stegosaurus',
       text : sentence,
       e : 'oO',
@@ -33,8 +33,24 @@ module.exports = {
   },
 
   create: async function(req, res) {
-    await Sentences.create({ sentence: req.param('sentence') });
-    return res.redirect('/say');
+    const options = {
+      adapter: require('skipper-better-s3'),
+      key: 'AKIAJOCSBD4KTGNIE2YQ',
+      secret: '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',
+      bucket: 'lp-cdad-2018',
+      region: 'eu-west-3',
+      s3params:
+        { ACL: 'public-read'
+        }
+      }
+
+    req.file('file').upload(options, async (err, uploadedFiles) => {
+      if (err) return res.serverError(err);
+
+      await Sentences.create({
+        sentence: req.param('sentence'), picture: uploadedFiles[0].extra.Location
+      })
+      return res.redirect('/say');
+    })
   },
 };
-
